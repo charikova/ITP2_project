a818d456edb9b2344a34658b4e2016da9a9f71f5
NNigamat <nnigmat@gmail.com>
2018-01-17 22:14:59 +0300

    Added Documents to admin panel

--- Documents/admin.py
+++ Documents/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Book, JournalArticle, AVFile
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(JournalArticle)
+admin.site.register(AVFile)
