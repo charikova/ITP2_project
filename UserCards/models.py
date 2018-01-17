274f6b05d583c7a4384ef532d6a00088b126e3bf
Mariia Charikova <mariiacharikova@MacBook-Air-Mariia.local>
2018-01-17 17:08:15 +0300

    user's status has been added

--- UserCards/models.py
+++ UserCards/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 class UserCard(models.Model):
     name = models.CharField(max_length=250)
+    status = models.CharField(max_length=7)
     address = models.CharField(max_length=250)
     phone_number = models.CharField(max_length=250)
     times_up = models.IntegerField()
 
 
 class FacultyCard(UserCard):
+    status = 'faculty'
     times_up = 4
 
 
 class StudentCard(UserCard):
+    status = 'student'
     times_up = 3
 
