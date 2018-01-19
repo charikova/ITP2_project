90313b90dd4ac85f555b8158b4b1b6c477fcd292
NNigamat <nnigmat@gmail.com>
2018-01-19 21:03:32 +0300

    Added static folder for css and javascript

--- UserCards/urls.py
+++ UserCards/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^signup$', views.signup, name='signup'),
+    url(r'^signup$/', views.signup, name='signup'),
     url(r"^$", views.index, name='index'),
-    url(r'^make_user$', views.make_user, name='make_user'),
+    url(r'^make_user/$', views.make_user, name='make_user'),
 ]
