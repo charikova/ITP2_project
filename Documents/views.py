917d016f6277159b49e314532ef99e9ad7b94c46
Danil <danilgin@inbox.ru>
2018-01-19 16:05:28 +0300

    added reset button to documents system choosing. Now working with paginator

--- Documents/views.py
+++ Documents/views.py
@@ -1,14 +1,19 @@
-from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render
 from .models import *
 
 def index(request):
-    documents = Document.objects.all()
-    print(request.POST)
-    return render(request, 'Documents/index.html', {'documents': documents})
+    documents = Paginator(Document.objects.all(), 10)
+    chosen_docs = [Document.objects.get(id=int(id)) for id in request.POST.keys() if id.isdigit()]
+    try:
+        page_num = request.GET['page']
+        if not documents.page(page_num):
+            raise EmptyPage
+    except (KeyError, EmptyPage):
+        page_num = 1
+    print(chosen_docs)
+    return render(request, 'Documents/index.html', {'documents': documents.page(page_num)})
 
 
 def show_doc_inf(request, doc_id):
-    http = Document.objects.filter(id=doc_id)
-    print(http)
-    return HttpResponse(http)
+    return render(request, 'Documents/doc_inf.html', {'doc': Document.objects.get(id=doc_id)})
