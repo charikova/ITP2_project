8dcf5dbacbc2497c4f1fdbc0db8262cec3754918
Danil <danilgin@inbox.ru>
2018-01-29 18:38:00 +0300

    added separated html for delete document feature

--- Documents/librarian_view.py
+++ Documents/librarian_view.py
@@ -45,23 +45,7 @@ class ModifyDocument:
 
 
 class DeleteDocument(ModifyDocument, DeleteView):
-    pass
-
-
-@need_to_be_staff
-def update_doc(request, pk):
-    doc = get_doc(request, pk)
-    fields = get_fields_of(doc)
-    if request.method == "GET":
-        return render(request, 'Documents/update_doc.html', 
-                      {'doc': doc, 'cover': doc.__dict__['cover'], 'fields': fields})
-    elif request.method == "POST":
-        print(request.POST)
-        for field in fields:
-            exec('doc.{0} = request.POST["{0}"]'.format(field[0]))
-        doc.save()
-        print(doc)
-        return redirect('../')
+    template_name = 'Documents/del_doc.html'
 
 
 def get_fields_of(doc, excess_fields=['document_ptr_id', '_state', 'id']):
@@ -71,7 +55,6 @@ def get_fields_of(doc, excess_fields=['document_ptr_id', '_state', 'id']):
             fields[key] = value
     fields = list(
         map(lambda key: (key, str(fields[key])), fields))
-    print(fields)
     return fields
 
 
@@ -91,8 +74,34 @@ class CreateDocument(ModifyDocument, CreateView):
         self.extra_context = {'model': self.model.type}
         return super(CreateDocument, self).get(request, *args, **kwargs)
 
+# def create_doc(request):
+#     if request.method == "GET":
+#         type = request.GET['type']
+#         model = None
+#         for cls in Document.__subclasses__():
+#             if cls.type == type:
+#                 model = cls
+#         fields = list(map(lambda x: x.replace(' ', '').replace(')', ''), model.__dict__['__doc__'].split(',')))[1:]
+#         fields.remove('type')
+#         return render(request, "Documents/create_doc.html", {'fields': fields, 'model': model.type})
+#     elif request.method == "POST":
+#         pass
+
 
 @need_to_be_staff
 def add_doc(request):
-    return render(request, 'Documents/add_doc.html', {'clss': list(map(lambda x: x.type, Document.__subclasses__()))})
+    return render(request, 'Documents/add_doc.html', {'clss': list(map(lambda x: x.type, Document.__subclasses__()))[:-1]})
 
+
+@need_to_be_staff
+def update_doc(request, pk):
+    doc = get_doc(request, pk)
+    fields = get_fields_of(doc)
+    if request.method == "GET":
+        return render(request, 'Documents/update_doc.html',
+                      {'doc': doc, 'cover': doc.__dict__['cover'], 'fields': fields})
+    elif request.method == "POST":
+        for field in fields:
+            exec('doc.{0} = request.POST["{0}"]'.format(field[0]))
+        doc.save()
+        return redirect('../')
