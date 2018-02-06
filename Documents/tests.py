a41c50cbe274b2a5593cbb149d1f96a346d2c12b
Roman Bogachev <32034827+concrete13377@users.noreply.github.com>
2018-02-06 13:46:39 +0300

    added tc7-8, updated 4, 5, 6

--- Documents/tests.py
+++ Documents/tests.py
@@ -4,14 +4,13 @@ from django.http import HttpRequest, Http404
 from .models import *
 from Documents.views import checkout
 import datetime
-from UserCards.models import UserProfile
 from UserCards.views import user_card_info
 
 
 class IntroductionToProgrammingTestCase(TestCase):
 
-    def TC1(self):
-        # initial state
+    def test_TC1(self):
+        #initial state
         p = User.objects.create_user('username', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L')
         UserProfile.objects.create(user=p, phone_number=123, status='student', address='1-103')
         l = User.objects.create_user('username2', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L', is_staff=True)
@@ -30,34 +29,6 @@ class IntroductionToProgrammingTestCase(TestCase):
         self.assertIs(patron_has_one_copy, True)
         self.assertIs(library_has_one_copy, True)
 
-<<<<<<< HEAD
-    def TC2(self):
-        pass
-
-    def TC3(self):
-        self.assertIs()
-
-    def TC4(self):
-        f = User.objects.create_user('Faculty', 'fac@mail.ru', '123456qwerty', first_name='F', last_name='L')
-        UserProfile.objects.create(user=f, status='faculty', phone_number=896000, address='2-107')
-
-        s = User.objects.create_user('Student', 'stu@mail.ru', '123456qwerty', first_name='S', last_name='L')
-        UserProfile.objects.create(user=s, status='student', phone_number=796001, address='2-110')
-
-        #l = UserProfile.objects.create_user('username', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L', /
-                        # is_stuff=True)
-        b = Book.objects.create(title='title', price=0, publication_date=datetime.datetime.now(),
-                                edition=1, copies=2, authors='sadf', cover='cover', publisher='pub', is_bestseller=True)
-
-        request = HttpRequest()
-        request.method = 'GET'
-        request.user = f
-        checkout(request, b.id)
-
-        self.assertEqual(
-            (f.documentcopy_set.filter(doc=b)[0].returning_date - f.documentcopy_set.filter(doc=b)[0].date).days,
-            datetime.timedelta(days=14).days-1)
-=======
         # librarian see
         request = HttpRequest()
         request.method = "GET"
@@ -68,8 +39,12 @@ class IntroductionToProgrammingTestCase(TestCase):
         self.assertEqual(b.title in str(response.content), True) # there are exist title of this book in response content
 
 
-    def TC2(self):
+    def test_TC2(self):
         p = User.objects.create_user('username', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L')
+
+        librarian = User.objects.create_user('l', 'exampl23@mail.ru', '123456qwerty', first_name='F', last_name='L',
+                                             is_staff=True)
+        UserProfile.objects.create(user=librarian, phone_number=123, status='faculty', address='1-103')
 
         # doesn't have any books by author A
         have_book = len(Book.objects.filter(authors='A')) > 0
@@ -86,29 +61,17 @@ class IntroductionToProgrammingTestCase(TestCase):
             pass
         else:
             raise Exception('should raise 404')
->>>>>>> abb0a5d059dee8f34a3a3974c857281dec48e4d0
 
-    def TC5(self):
-        s1 = User.objects.create_user('Student1', 'Student1@mail.ru', '123456qwerty', first_name='Student1', last_name='L')
-        s2 = User.objects.create_user('Student2', 'Student2@mail.ru', '123456qwerty', first_name='Student2', last_name='L')
-        s3 = User.objects.create_user('Student3', 'Student3@mail.ru', '123456qwerty', first_name='Student3', last_name='L')
 
-<<<<<<< HEAD
-        UserProfile.objects.create(user=s1, status='student', phone_number=896000, address='2-107')
-        UserProfile.objects.create(user=s2, status='student', phone_number=896000, address='2-107')
-        UserProfile.objects.create(user=s3, status='student', phone_number=896000, address='2-107')
-
-        #l = User.objects.create_user('username', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L',/
-                                     #is_stuff=True)
-
-        b = Book.objects.create(title='title', price=0, publication_date=datetime.datetime.now(),
-                                edition=1, copies=2, authors='sadf', cover='cover', publisher='pub', is_bestseller=True)
-=======
-    def TC3(self):
+    def test_TC3(self):
         student = User.objects.create_user('s', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L')
         faculty = User.objects.create_user('f', 'exampl2@mail.ru', '123456qwerty', first_name='F', last_name='L')
+        librarian = User.objects.create_user('l', 'exampl23@mail.ru', '123456qwerty', first_name='F', last_name='L', is_staff=True)
+
         UserProfile.objects.create(user=student, phone_number=123, status='student', address='1-103')
         UserProfile.objects.create(user=faculty, phone_number=123, status='faculty', address='1-103')
+        UserProfile.objects.create(user=librarian, phone_number=123, status='faculty', address='1-103')
+
         book = Book.objects.create(title='title', price=0, publication_date=datetime.datetime.now(),
                                 edition=1, copies=2, authors='sadf', cover='cover', publisher='pub')
 
@@ -120,37 +83,64 @@ class IntroductionToProgrammingTestCase(TestCase):
         returning_date = faculty.documentcopy_set.get(doc=book).returning_date
         should_be_today = returning_date - datetime.timedelta(days=28)
         should_be_today = datetime.date(year=should_be_today.year, month=should_be_today.month, day=should_be_today.day)
+
         self.assertEqual(should_be_today, datetime.date.today())
 
-    def TC4(self):
-        pass
->>>>>>> abb0a5d059dee8f34a3a3974c857281dec48e4d0
+    def test_TC4(self):
+        f = User.objects.create_user('Faculty', 'fac@mail.ru', '123456qwerty', first_name='F', last_name='L')
+        UserProfile.objects.create(user=f, status='faculty', phone_number=896000, address='2-107')
+
+        s = User.objects.create_user('Student', 'stu@mail.ru', '123456qwerty', first_name='S', last_name='L')
+        UserProfile.objects.create(user=s, status='student', phone_number=796001, address='2-110')
+
+        b = Book.objects.create(title='title', price=0, publication_date=datetime.datetime.now(),
+                                edition=1, copies=2, authors='sadf', cover='cover', publisher='pub', is_bestseller=True)
+
+        request = HttpRequest()
+        request.method = 'GET'
+        request.user = f
+        checkout(request, b.id)
+
+        self.assertEqual(
+            (f.documentcopy_set.filter(doc=b)[0].returning_date - f.documentcopy_set.filter(doc=b)[0].date).days,
+            datetime.timedelta(days=14).days - 1)
+
+    def test_TC5(self):
+        s1 = User.objects.create_user('Student1', 'Student1@mail.ru', '123456qwerty', first_name='Student1',
+                                      last_name='L')
+        s2 = User.objects.create_user('Student2', 'Student2@mail.ru', '123456qwerty', first_name='Student2',
+                                      last_name='L')
+        s3 = User.objects.create_user('Student3', 'Student3@mail.ru', '123456qwerty', first_name='Student3',
+                                      last_name='L')
+
+        UserProfile.objects.create(user=s1, status='student', phone_number=896000, address='2-107')
+        UserProfile.objects.create(user=s2, status='student', phone_number=896000, address='2-107')
+        UserProfile.objects.create(user=s3, status='student', phone_number=896000, address='2-107')
+
+        # l = User.objects.create_user('username', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L',/
+        # is_stuff=True)
+
+        b = Book.objects.create(title='title', price=0, publication_date=datetime.datetime.now(),
+                                edition=1, copies=2, authors='sadf', cover='cover', publisher='pub', is_bestseller=True)
 
         request = HttpRequest()
         request.method = 'GET'
         request.user = s1
         checkout(request, b.id)
 
-<<<<<<< HEAD
         request.user = s2
         checkout(request, b.id)
-=======
-    def TC5(self):
-        pass
->>>>>>> abb0a5d059dee8f34a3a3974c857281dec48e4d0
 
         request.user = s3
         checkout(request, b.id)
 
         self.assertEqual(len(b.documentcopy_set.all()), 2)
 
-    def TC6(self):
-<<<<<<< HEAD
+
+    def test_TC6(self):
         p = User.objects.create_user('username', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L')
         UserProfile.objects.create(user=p, status='student', phone_number=896000, address='2-107')
 
-        # l = User.objects.create_user('username', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L', /
-                                     # is_stuff=True)
         b = Book.objects.create(title='title', price=0, publication_date=datetime.datetime.now(),
                                 edition=1, copies=2, authors='sadf', cover='cover', publisher='pub', is_bestseller=True)
 
@@ -163,21 +153,63 @@ class IntroductionToProgrammingTestCase(TestCase):
         self.assertEqual(len(b.documentcopy_set.all()), 1)
 
 
-=======
+    def test_TC7(self):
+        p1 = User.objects.create_user('s', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L')
+        p2 = User.objects.create_user('f', 'exampl2@mail.ru', '123456qwerty', first_name='F', last_name='L')
+        UserProfile.objects.create(user=p1, phone_number=123, status='student', address='1-103')
+        UserProfile.objects.create(user=p2, phone_number=123, status='student', address='1-103')
+
+        librarian = User.objects.create_user('l', 'exampl23@mail.ru', '123456qwerty', first_name='F', last_name='L',is_staff=True)
+        UserProfile.objects.create(user=librarian, phone_number=123, status='faculty', address='1-103')
+
+        b1 = Book.objects.create(title='b1', price=0, publication_date=datetime.datetime.now(),
+                                   edition=1, copies=2, authors='sadf', cover='cover', publisher='pub')
+
+        request = HttpRequest()
+        request.method = "GET"
+        request.user = p1
+        checkout(request, b1.id)
+
+        request = HttpRequest()
+        request.method = "GET"
+        request.user = p2
+        checkout(request, b1.id)
+
+        p1_has_one_copy = len(p1.documentcopy_set.filter(doc=b1)) == 1
+        p2_has_one_copy = len(p2.documentcopy_set.filter(doc=b1)) == 1
+        p1_and_p2 = p1_has_one_copy == p2_has_one_copy
+        library_has_no_copy = len(Document.objects.filter(title='title')) == 0
+
+        self.assertEqual(p1_and_p2, library_has_no_copy)
+
+
+    def test_TC8(self):
+        student = User.objects.create_user('s', 'exampl@mail.ru', '123456qwerty', first_name='F', last_name='L')
+        faculty = User.objects.create_user('f', 'exampl2@mail.ru', '123456qwerty', first_name='F', last_name='L')
+        librarian = User.objects.create_user('l', 'exampl23@mail.ru', '123456qwerty', first_name='F', last_name='L',
+                                             is_staff=True)
+
+        UserProfile.objects.create(user=student, phone_number=123, status='student', address='1-103')
+        UserProfile.objects.create(user=faculty, phone_number=123, status='faculty', address='1-103')
+        UserProfile.objects.create(user=librarian, phone_number=123, status='faculty', address='1-103')
+
+        book = Book.objects.create(title='title', price=0, publication_date=datetime.datetime.now(),
+                                   edition=1, copies=2, authors='sadf', cover='cover', publisher='pub')
+        request = HttpRequest()
+        request.method = "GET"
+        request.user = student
+        checkout(request, book.id)
+
+        returning_date = student.documentcopy_set.get(doc=book).returning_date
+        should_be_today = returning_date - datetime.timedelta(days=21)
+        should_be_today = datetime.date(year=should_be_today.year, month=should_be_today.month, day=should_be_today.day)
+
+        self.assertEqual(should_be_today, datetime.date.today())
+
+
+    def test_TC9(self):
         pass
->>>>>>> abb0a5d059dee8f34a3a3974c857281dec48e4d0
 
 
-    def TC7(self):
+    def test_TC10(self):
         pass
-
-    def TC8(self):
-        pass
-
-    def TC9(self):
-        pass
-
-    def TC10(self):
-        pass
-
-
