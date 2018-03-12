90e1feb093dbc2ae751d64ec9de6a6cd5268110b
Danil <danilgin@inbox.ru>
2018-03-13 01:49:04 +0300

    new version of requestmodel: users

--- BookRequests/models.py
+++ BookRequests/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 
 class Request(models.Model):
     doc = models.ForeignKey(Document, null=True, default=None, on_delete=models.CASCADE)
-    # checked_up_by_whom = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
     users = models.ManyToManyField(User)  # you may think of it as array of users
     timestamp = models.DateTimeField(auto_now_add=True)
 
