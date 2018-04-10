736c34501280d592327d42664a630f00b8171112
Nnigmat <nnigmat@gmail.com>
2018-04-10 20:11:37 +0300

    Add groups to admin site and add field privileges to UserCards/models.py

--- Documents/admin.py
+++ Documents/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+'''
 admin.site.register(Book)
 admin.site.register(JournalArticle)
 admin.site.register(AVFile)
 admin.site.register(DocumentCopy)
 admin.site.register(Document)
+'''
--- UserCards/admin.py
+++ UserCards/admin.py
@@ -1,9 +1,15 @@
-from django.contrib import admin
 from .models import UserProfile
+from django.contrib.auth.models import Group
+from django.contrib import admin
+# Need to import this since auth models get registered on import.
+import django.contrib.auth.admin
+import django.contrib.auth.models
+from django.contrib import auth
 
 
 class AdminProfile(admin.ModelAdmin):
-    list_display = ['user', 'status', 'phone_number', 'address']
+    list_display = ['user', 'status', 'phone_number', 'address', 'privileges']
 
+admin.site.unregister(Group)
 
 admin.site.register(UserProfile, AdminProfile)
